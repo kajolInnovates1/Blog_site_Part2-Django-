d692349a4f94c9ffd6872882f57d1b55a05e495a
kajol201 <mdraselislamkajol201@gmail.com>
2025-10-01 18:27:13 +0600

    final commit

--- BlogProject_part2/urls.py
+++ BlogProject_part2/urls.py
@@ -22,6 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('category/<slug:category_slug>/',views.home,name='category_wise_post'),
     path('author/',include('authors.urls')),
     path('category/',include('categories.urls')),
     path('post/',include('posts.urls')),
